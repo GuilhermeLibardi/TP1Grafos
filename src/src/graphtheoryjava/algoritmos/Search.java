e50fde51d3f7c2ada1559868785be0f988b7cd6e
Guilherme Libardi <guilherme.libardig@gmail.com>
2018-01-11 18:34:06 -0200

    Busca em profundidade

--- src/src/graphtheoryjava/algoritmos/Search.java
+++ src/src/graphtheoryjava/algoritmos/Search.java
@@ -27,7 +27,7 @@ public class Search {
         }
     }
 
-    public ArrayList<Integer> buscaLargura(int s) {
+    public ArrayList<Integer> BuscaLargura(int s) {
         ArrayList<Integer> fila = new ArrayList<Integer>();
         this.reset();
 
@@ -50,32 +50,107 @@ public class Search {
         return this.ordem;
     }
 
-    public ArrayList<Integer> buscaProfundidade(int s) {
+    public ArrayList<Integer> BuscaProfundidade(int s) {
         Pilha pilha = new Pilha();
         Info info = new Info(this.grafo);
-        int u;
+        int u, i = 0;
         this.reset();
 
         this.visitado[s] = 1;
+        this.ordem.add(s);
         pilha.Empilha(s);
 
         while (!pilha.isEmpty()) {
-            u = (int) pilha.Desempilha();
-            pilha.Empilha(u);
+            u = (int) pilha.lookFirst();
             ArrayList<Integer> adjacentes = info.sucessores(u);
             if (!adjacentes.isEmpty()) {
                 for (int v : adjacentes) {
-                    if(this.visitado[v] == 0){
+                    if (this.visitado[v] == 0) {
                         this.visitado[v] = 1;
                         pilha.Empilha(v);
                         this.ordem.add(v);
-                    } else {
+                        break;
+                    } else if (adjacentes.size() - 1 == i) {
                         pilha.Desempilha();
                     }
+                    i++;
                 }
+            } else {
+                pilha.Desempilha();
             }
+            i = 0;
         }
         return this.ordem;
+    }
+
+    public ArrayList<Integer> BuscaProfRecursiva(int s) {
+        this.reset();
+        ArrayList<Integer> retorno = new ArrayList<>(0);
+        for (int i : this.grafo.vertices) {
+            if (this.visitado[i] == 0) {
+                PROF(i);
+                retorno.add(i);
+            }
+        }
+        return retorno;
+    }
+
+    public int ComponentesConexas() {
+        this.reset();
+        int componente = 0;
+
+        for (int i : this.visitado) {
+            if (visitado[i] == 0) {
+                componente++;
+                this.PROF_CON(i, componente);
+            }
+        }
+
+        return componente;
+    }
+
+    public ArrayList<Integer> OrdenacaoTopologica() {
+        ArrayList<Integer> retorno = new ArrayList<>(0);
+        this.reset();
+
+        for (int i : this.grafo.vertices) {
+            if (visitado[i] == 0) {
+                PROF_ORD(i, retorno);
+            }
+        }
+        return retorno;
+    }
+
+    private void PROF_ORD(int u, ArrayList<Integer> retorno) {
+        Info info = new Info(this.grafo);
+        visitado[u] = 1;
+        for (int v : info.sucessores(u)) {
+            if (visitado[v] == 0) {
+                PROF_ORD(v, retorno);
+            }
+        }
+        retorno.add(0, u);
+    }
+
+    private void PROF_CON(int u, int marca) {
+        Info info = new Info(this.grafo);
+        this.visitado[u] = marca;
+        for (int v : info.sucessores(u)) {
+            if (this.visitado[v] == 0) {
+                this.PROF_CON(v, marca);
+            }
+        }
+    }
+
+    private void PROF(int u) {
+        Info info = new Info(this.grafo);
+
+        this.visitado[u] = 1;
+        for (int v : info.sucessores(u)) {
+            if (visitado[v] == 0) {
+                PROF(v);
+            }
+        }
     }
 
 //    public ArrayList<Integer> buscaProfundidadeRec(int u);;
