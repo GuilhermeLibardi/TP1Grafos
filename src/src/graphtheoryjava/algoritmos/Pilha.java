dca4138cf2a8b714742c59b78d0855485705a32a
Guilherme Libardi <guilherme.libardig@gmail.com>
2018-01-11 19:04:32 -0200

    Busca em largura

--- src/src/graphtheoryjava/algoritmos/Pilha.java
+++ src/src/graphtheoryjava/algoritmos/Pilha.java
@@ -4,17 +4,22 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Pilha {
+
     private List<Object> objs = new LinkedList<>();
-    
-    public void Empilha(Object objeto){
+
+    public void Empilha(Object objeto) {
         this.objs.add(objeto);
     }
-    
-    public Object Desempilha(){
+
+    public Object Desempilha() {
         return this.objs.remove(this.objs.size() - 1);
     }
-    
-    public boolean isEmpty(){
+
+    public boolean isEmpty() {
         return this.objs.isEmpty();
+    }
+
+    public Object lookFirst() {
+        return this.objs.get(this.objs.size() - 1);
     }
 }
